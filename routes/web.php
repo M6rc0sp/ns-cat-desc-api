4c956f5f7d92e8759f3cecfbdbdcea5f25054f97
m6rc0sp <paulo.marcos0108@live.com>
2026-01-20 10:40:54 -0300

    refactor: Reorganizar rotas de instalação e descrições no arquivo web.php

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,10 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-// Rota pública de instalação do app Nuvemshop
-$router->get('/ns/install', 'NuvemshopController@install');
-
-// Rotas públicas para consumir descrições (para frontend ou widgets)
-$router->group(['prefix' => 'public'], function () use ($router) {
-    $router->get('/descriptions/{categoryId}', 'DescriptionController@getCategoryDescription');
-    $router->get('/descriptions', 'DescriptionController@getCategoriesDescriptions');
-});
-
 $router->group(['prefix' => 'api'], function () use ($router) {
+    // Rota pública de instalação do app Nuvemshop
+    $router->get('/ns/install', 'NuvemshopController@install');
+
     $router->group(['prefix' => 'descriptions'], function () use ($router) {
         $router->get('/', 'DescriptionController@index');
         $router->get('/categories', 'DescriptionController@getCategories');
@@ -36,4 +30,10 @@ $router->group(['prefix' => 'api'], function () use ($router) {
         $router->put('/{id}', 'DescriptionController@update');
         $router->delete('/{id}', 'DescriptionController@destroy');
     });
+});
+
+// Rotas públicas para consumir descrições (para frontend ou widgets)
+$router->group(['prefix' => 'public'], function () use ($router) {
+    $router->get('/descriptions/{categoryId}', 'DescriptionController@getCategoryDescription');
+    $router->get('/descriptions', 'DescriptionController@getCategoriesDescriptions');
 });
